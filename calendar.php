03b73fe8ef7518846fa8ac6aa548a8989240697a
Mahmoud Magdy Hegazi <55125302+MahmoudHegazi@users.noreply.github.com>
2020-12-07 01:25:48 +0100

    Update calendar.php

--- calendar.php
+++ calendar.php
@@ -2,26 +2,34 @@
 <html>
 <body>
 <?php
-//$d=cal_days_in_month(CAL_GREGORIAN,2,1965);
-//echo "There was $d days in February 1965.<br>";
 
 
 function get_months_array($month_class, $year) {
 $months = array("January", "February", "March","April","May","June","July","August","September","October","November","December");
 
 
-foreach ($months as $value) {
-  echo "<li class=" . $month_class . '" data-year="'. $year .'">' . $value . '</li>';
-}
 
 
+
+$month_len = count($months);
+
+function get_month_days($month_index,$year) {
+$days_in_month =cal_days_in_month(CAL_GREGORIAN,$month_index,$year);
+  return $days_in_month;
 }
+
+for ($i=0; $i < $month_len; $i++) {
+   echo '<li class="month_in_' . $month_class . '" data-year="' . $month_class . '">' . 
+   $months[$i] . ' ' . get_month_days($i+1,$year) . '</li>';
+}
+}
+
 
 
 ?>
 
 <ul id="my_menu">
-  <?php echo get_months_array('year1', 2019); ?>
+  <?php  get_months_array('year1', 2019); ?>
 </ul>
 
 
